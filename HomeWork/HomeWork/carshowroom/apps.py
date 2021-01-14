de843051c813b812c640934b7acaed77b755fdda
Enjoy4e <55601347+Enjoy4e@users.noreply.github.com>
2021-01-14 22:05:33 +0300

    Delete HomeWork directory

--- HomeWork/HomeWork/carshowroom/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CarshowroomConfig(AppConfig):
-    name = 'carshowroom'
--- HomeWork/HomeWork/carshowroom/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'carshowroom'
-urlpatterns = [
-    path('', views.cars, name ="home"),
-    path('report/',views.report, name = "report"),
-    path('suppliers/', views.supply, name="supply"),
-]
